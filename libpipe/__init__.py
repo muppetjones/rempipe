1b10b8a8aa149647b4c8936ed1c665382e05cc12
Stephen J Bush <muppetjones@gmail.com>
2016-03-30 15:42:28 -0500

    removed auto import/add to path from libpipe.__init__

--- libpipe/__init__.py
+++ libpipe/__init__.py
@@ -1,6 +1,6 @@
-import sys
-from os.path import dirname, abspath, join
+# import sys
+# from os.path import dirname, abspath, join
 
-print("libpipe init -- add 'remsci' to path")
-sys.path.insert(
-    0, join(dirname(dirname(dirname(abspath(__file__)))), 'remsci'))
+# print("libpipe init -- add 'remsci' to path")
+# sys.path.insert(
+#     0, join(dirname(dirname(dirname(abspath(__file__)))), 'remsci'))
