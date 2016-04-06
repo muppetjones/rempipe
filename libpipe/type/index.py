04a2cade37bc1822c8672c42979219413bdc340f
Stephen J Bush <muppetjones@gmail.com>
2016-04-05 21:39:38 -0500

    IndexType + tests done (cleaner, leaner than before); need to finish integration into Cmd

--- libpipe/type/index.py
+++ libpipe/type/index.py
@@ -9,14 +9,14 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def factory(name='IndexSubType', extn=[], counts=[]):
+def factory(name='IndexSubType', extns=[], counts=[]):
     if not counts:
-        counts = [1] * len(extn)  # assume only one of each extn required!
-    elif counts and len(extn) != len(counts):
-        msg = 'Unequal num of extn ({}) vs counts ({})'.format(
-            len(extn), len(counts))
+        counts = [1] * len(extns)  # assume only one of each extn required!
+    elif counts and len(extns) != len(counts):
+        msg = 'Unequal num of extns ({}) vs counts ({})'.format(
+            len(extns), len(counts))
         raise ValueError(msg)
-    _subcls = type(name, (IndexBase, ), dict(extn=extn, counts=counts))
+    _subcls = type(name, (str, IndexType, ), dict(extns=extns, counts=counts))
     return _subcls
 
 
@@ -24,13 +24,7 @@ class IndexMeta(base.TypeMeta):
     pass
 
 
-class IndexBase(metaclass=IndexMeta):
-
-    def __init__(self, stem):
-        pass
-
-
-class IndexType(str, base.TypeBase):
+class IndexType(metaclass=IndexMeta):
 
     '''Alignment index type, e.g., hisat2 index
 
@@ -39,123 +33,38 @@ class IndexType(str, base.TypeBase):
     Attributes:
         extns: A list of expected extensions for the index.
         counts: A list of expected file counts for each extension.
-        _path: The directory where the index is found.
-        _prefix: The "full" path to the index (path + name)
-        _name: The basename of the index.
+        path: The directory where the index is found.
+        name: The basename of the index.
 
     Examples:
         # For a genome with a hisat2 index named GRCh38p5, i.e., GRCh38p5.*.ht2
         index_class = IndexType.factory({'.ht2': 8}, name="Hisat2Index")
         index_obj = index_class("path/to/genome/GRCh38p5")
-        print(index_obj._path)
-        print(index_obj._prefix)
-        print(index_obj._name)
+        print(index_obj)
+        print(index_obj.path)
+        print(index_obj.name)
 
-            > path/to/genome
-            > path/to/genome/GRCh38p5
-            > GRCh38p5
+        > path/to/genome/GRCh38p5
+        > path/to/genome
+        > GRCh38p5
     '''
 
-    counts = []
-    extns = []
+    def __init__(self, value=None):
 
-    def __init__(self, _path=None):
-        self._path = _path
-        self._prefix = None
-        self._name = None
-
-        self._check_prefix()
-        self._check_extns()  # raise ValueError if unexpected file count!
-
-    #
-    #   magic methods
-    #
-
-    def __str__(self):
-        return self._prefix or self._path
-
-    def __eq__(self, other):
-        return str(self) == other
-
-    #
-    #   Check methods
-    #
-
-    def _check_prefix(self):
-        try:
-            _is_full = os.path.isdir(self._path)
-            _is_partial = os.path.isdir(os.path.dirname(self._path))
-            if not _is_full and _is_partial:
-                self._prefix = self._path
-                self._path, self._name = os.path.split(self._prefix)
-        except TypeError:
-            pass  # probably given None
+        if value:
+            self.path, self.name = os.path.split(value)
+            self._check_extns()
 
     def _check_extns(self):
-        pattern = [self._name] if self._name is not None else []
-        _files = path.walk_file(
-            self._path, extension=self.extns, pattern=pattern)
+        files = path.walk_file(
+            self.path, extension=self.extns, pattern=[self.name])
 
         errs = []
         for extn, cnt in zip(self.extns, self.counts):
-            _found = [f for f in _files if f.endswith(extn)]
-            if len(_found) != cnt:
-                errs.append('{}: {} v {}'.format(extn, len(_found), cnt))
+            matched = [f for f in files if f.endswith(extn)]
+            if len(matched) != cnt:
+                errs.append('{}: {} v {}'.format(extn, len(matched), cnt))
+
         if errs:
             msg = 'Bad index: {}'.format('; '.join(errs))
             raise ValueError(msg)
-
-    #
-    #   fatory
-    #
-
-    @classmethod
-    def factory(*args, name=None, **kwargs):
-        '''Create an IndexType with a defined set of extensions.
-
-        Define and IndexType that expects a given number of the expected
-        extensions.
-
-        Example:
-            index_class = IndexType.factory('.fq', '.fastq')
-        Example:
-            index_class = IndexType.factory(['.fq', '.fastq'])
-        Example:
-            index_class = IndexType.factory({'.bt2': 4, '.rev.bt2': 2})
-        Example:
-            index_class = IndexType.factory(**{'.bt2': 4, '.rev.bt2': 2})
-        '''
-
-        # TODO(sjbush): consider making a full metaclass
-
-        _counts = []
-        _extns = []
-
-        if len(args) > 1:
-            _extns = sorted(list(args))
-        elif len(args) == 1:
-            _extns = args[0]
-
-        if kwargs:
-            _extns = kwargs
-
-        if isinstance(_extns, dict):
-            _extns_sorted = sorted(list(_extns.keys()))
-            _counts = [_extns[v] for v in _extns_sorted]
-            _extns = _extns_sorted
-        else:
-            try:
-                _extns = sorted(_extns)
-            except TypeError:
-                pass  # we don't care if it's not iterable
-
-        if not _counts:
-            _counts = [1] * len(_extns)
-
-        name = name if name else 'IndexSubType'
-        _subcls = type(name, (IndexType, ), dict(
-            extns=_extns,
-            counts=_counts
-        ))
-
-        return _subcls
--- tests/type/test_index.py
+++ tests/type/test_index.py
@@ -1,5 +1,5 @@
 
-import unittest
+import os.path
 from unittest import mock
 
 
@@ -17,59 +17,14 @@ class IndexTypeTestCase(base.LibpipeTestCase):
 
     def setUp(self):
         super().setUp()
-        self.PARENT = index.IndexBase
+        self.PARENT = index.IndexType
         self.FACTORY = index.factory
         self.CHILD = self.FACTORY('IndexSubType')
-
-
-class IndexTypeTestCase__deprecated(base.LibpipeTestCase):
-
-    def setUp(self):
-        # create our test index
-        extns = {'.foo': 4, '.bar': 2}
-        self.INDEX = index.IndexType.factory(extns)
-
-        # setup example output from walk_file (or walk_safe)
-        n_foo, n_bar = (4, 2)
-        self.default_file_list = (
-            ['{}.foo'.format(i) for i in range(n_foo)] +
-            ['{}.bar'.format(i) for i in range(n_bar)]
-        )
-
-    def setup_mock_walk_safe(self, retval=None):
-        patcher = mock.patch('libpipe.util.path.walk_safe')
-        m = patcher.start()
-        if not retval:
-            retval = {'file': list(self.default_file_list)}
-        m.return_value = retval
-        self.addCleanup(patcher.stop)
-        return m
-
-    def setup_mock_walk_file(self, retval=None):
-        patcher = mock.patch('libpipe.util.path.walk_file')
-        m = patcher.start()
-        if not retval:
-            retval = list(self.default_file_list)
-        m.return_value = retval
-        self.addCleanup(patcher.stop)
-        return m
-
-    def setup_mock_check_prefix(self):
-        patcher = mock.patch.object(index.IndexType, '_check_prefix')
-        m = patcher.start()
-        self.addCleanup(patcher.stop)
-        return m
-
-    def setup_mock_check_extns(self):
-        patcher = mock.patch.object(index.IndexType, '_check_extns')
-        m = patcher.start()
-        self.addCleanup(patcher.stop)
-        return m
-
 
 #
 # test base and factory
 #
+
 
 class TestIndexMeta(base.LibpipeTestCase):
 
@@ -85,17 +40,17 @@ class TestIndexType__TypeBase(test_base.TestTypeBase):
 
     def setUp(self):
         super().setUp()
-        self.PARENT = index.IndexBase
+        self.PARENT = index.IndexType
         self.FACTORY = index.factory
         self.CHILD = self.FACTORY('IndexSubType')
 
-        patcher = mock.patch.object(index.IndexType, '_check_prefix')
+        patcher = mock.patch('libpipe.util.path.walk_file')
         patcher.start()
         self.addCleanup(patcher.stop)
 
-        patcher = mock.patch.object(index.IndexType, '_check_extns')
-        patcher.start()
-        self.addCleanup(patcher.stop)
+        # patcher = mock.patch.object(index.IndexType, '_check_extns')
+        # patcher.start()
+        # self.addCleanup(patcher.stop)
 
     def test_factory_sets_child_name(self):
         child_class = self.FACTORY(name='Dolly')
@@ -109,135 +64,106 @@ class TestIndexFactory(IndexTypeTestCase):
 
     def test_factory_stores_extensions_on_child_class(self):
         extn = ['.foo', '.bar']
-        child = self.FACTORY(extn=extn)
-        self.assertEqual(child.extn, extn)
+        child = self.FACTORY(extns=extn)
+        self.assertEqual(child.extns, extn)
 
     def test_factory_stores_counts_on_child_class(self):
         extn = ['.foo', '.bar']
         counts = [3, 5]
-        child = self.FACTORY(extn=extn, counts=counts)
+        child = self.FACTORY(extns=extn, counts=counts)
         self.assertEqual(child.counts, counts)
 
     def test_factory_assumes_all_counts_eq_1_iff_not_given(self):
         extn = ['.foo', '.bar']
         counts = [1] * len(extn)
-        child = self.FACTORY(extn=extn)
+        child = self.FACTORY(extns=extn)
         self.assertEqual(child.counts, counts)
 
     def test_factory_raises_ValueError_if_n_counts_ne_n_extn(self):
         extn = ['.foo', '.bar']
         counts = [1, ]
         with self.assertRaises(ValueError):
-            self.FACTORY(extn=extn, counts=counts)
+            self.FACTORY(extns=extn, counts=counts)
 
 
-def TestIndexSubType(IndexTypeTestCase):
+class TestIndexSubType(IndexTypeTestCase):
 
     def setUp(self):
         super().setUp()
+        self.child = self.FACTORY(
+            name='TestIndex', extns=['.bar', '.foo'], counts=[2, 4])
 
+        n_foo, n_bar = (4, 2)
 
-@unittest.skip('all')
-class TestIndexType(IndexTypeTestCase):
+        self.default_file_list = (
+            ['{}.foo'.format(i) for i in range(n_foo)] +
+            ['{}.bar'.format(i) for i in range(n_bar)]
+        )
 
-    '''Test that IndexType fulfills TypeBase tests'''
+    #
+    #   Mock setup
+    #
 
-    def setUp(self):
-        self.TYPE = index.IndexType
-        extns = {'.foo': 4, '.bar': 2}
-        self.INDEX = self.TYPE.factory(extns)
+    def setup_mock_walk_safe(self, retval=None):
+        patcher = mock.patch('libpipe.util.path.walk_safe')
+        m = patcher.start()
+        if not retval:
+            retval = {'file': list(self.default_file_list)}
+            m.return_value = retval
+            self.addCleanup(patcher.stop)
+            return m
 
-        self.setup_mock_check_extns()
-        self.setup_mock_check_prefix()
+    def setup_mock_walk_file(self, retval=None):
+        patcher = mock.patch('libpipe.util.path.walk_file')
+        m = patcher.start()
+        if not retval:
+            retval = list(self.default_file_list)
+            m.return_value = retval
+            self.addCleanup(patcher.stop)
+            return m
 
-    def test_str_index_returns_given_file(self):
-        _index = self.INDEX('foo/bar')
-        self.assertEqual(str(_index), 'foo/bar')
+    def setup_mock_check_extns(self, obj=None):
+        obj = obj if obj else self.CHILD
+        patcher = mock.patch.object(obj, '_check_extns')
+        m = patcher.start()
+        self.addCleanup(patcher.stop)
+        return m
+
+    #
+    #   Tests
+    #
+
+    def test_is_subclassed_from_str(self):
+        self.assertTrue(issubclass(self.child, str), 'Not subclassed from str')
+
+    def test_init_with_no_args_returns_empty_string(self):
+        self.assertEqual(self.child(), '')
 
     def test_eq_returns_True_for_given_str(self):
-        _index = self.INDEX('foo/bar')
+        self.setup_mock_check_extns(self.child)
+
+        _index = self.child('foo/bar')
         self.assertEqual(_index, 'foo/bar')
 
-    def test_instance_is_string(self):
-        _index = self.INDEX('foo/bar')
-        _types = [str, self.TYPE, self.INDEX]
-        for _t in _types:
-            with self.subTest(_type=_t):
-                self.assertIsInstance(_index, _t)
+    def test_init_sets_path_from_dirname_of_given(self):
+        self.setup_mock_check_extns(self.child)
+        path = 'foo/bar'
+        index = self.child(path)
+        self.assertEqual(index.path, os.path.dirname(path))
 
-
-@unittest.skip('all')
-class TestIndexType_factory(base.LibpipeTestCase):
-
-    def test_factory_sets_expected_extns_attr_to_empty_list(self):
-        index_obj = index.IndexType.factory()
-        self.assertEqual(index_obj.extns, [])
-
-    def test_factory_sets_expected_counts_attr_to_empty_list(self):
-        index_obj = index.IndexType.factory()
-        self.assertEqual(index_obj.counts, [])
-
-    def test_factory_list_sets_extns_in_alpha_order(self):
-        expected = ['.rev.bt2', '.bt2', ]
-        _index = index.IndexType.factory(expected)
-        self.assertEqual(_index.extns, sorted(expected))
-
-    def test_factory_multi_sets_extns_in_alpha_order(self):
-        expected = ['.rev.bt2', '.bt2', ]
-        _index = index.IndexType.factory(*expected)
-        self.assertEqual(_index.extns, sorted(expected))
-
-    def test_factory_sets_expected_attr_from_kwargs(self):
-        extn_dict = {'.bt2': 4, '.rev.bt2': 2, '.foo': None, '.bar': None}
-
-        # repeat check multiple times to ensure consistent
-        for i in range(50):
-            _index = index.IndexType.factory(**extn_dict)
-
-            # extns
-            expected = sorted(list(extn_dict.keys()))
-            self.assertEqual(_index.extns, expected)
-
-            # counts
-            expected = [extn_dict[k] for k in sorted(list(extn_dict.keys()))]
-            self.assertEqual(_index.counts, expected)
-
-    def test_factory_dict_sets_extns_in_alpha_order(self):
-        extn_dict = {'.bt2': 4, '.rev.bt2': 2, '.foo': None, '.bar': None}
-
-        # repeat check multiple times to ensure consistent
-        for i in range(50):
-            _index = index.IndexType.factory(extn_dict)
-            expected = sorted(list(extn_dict.keys()))
-            self.assertEqual(_index.extns, expected)
-
-    def test_factory_dict_sets_counts_in_extn_order(self):
-        extn_dict = {'.bt2': 4, '.rev.bt2': 2, '.foo': None, '.bar': None}
-
-        # repeat check multiple times to ensure consistent
-        for i in range(50):
-            _index = index.IndexType.factory(extn_dict)
-            expected = [extn_dict[k] for k in sorted(list(extn_dict.keys()))]
-            self.assertEqual(_index.counts, expected)
-
-    def test_factory_sets_counts_to_1_if_not_given(self):
-        extns = ['.rev.bt2', '.bt2', ]
-        _index = index.IndexType.factory(*extns)
-        expected = [1 for e in extns]
-        self.assertEqual(_index.counts, expected)
-
-
-#
-#   Test index checking
-#
-
-@unittest.skip('all')
-class TestIndexType__check_extns(IndexTypeTestCase):
+    def test_init_sets_name_from_basename_of_given(self):
+        self.setup_mock_check_extns(self.child)
+        path = 'foo/bar'
+        index = self.child(path)
+        self.assertEqual(index.name, os.path.basename(path))
 
     def test_init_calls_walk_file_with_extensions(self):
         mock_walk = self.setup_mock_walk_file()
-        _index = self.INDEX('foo/bar')
-        expected = mock.call('foo/bar', extension=_index.extns, pattern=[])
+
+        _index = self.child('foo/bar')
+
+        expected = mock.call(
+            'foo', extension=_index.extns, pattern=['bar'])
         mock_walk.assert_has_calls([expected])
 
     def test_init_raises_ValueError_if_n_files_w_extn_ne_counts(self):
@@ -250,41 +176,4 @@ class TestIndexType__check_extns(IndexTypeTestCase):
                 ['{}.bar'.format(i) for i in range(n_bar)]
             )
             with self.assertRaises(ValueError):
-                self.INDEX('test/dir')
-
-    def test_name_set_as_pattern_for_walk_file(self):
-        mock_walk = self.setup_mock_walk_file()
-        with mock.patch('os.path.isdir', side_effect=[False, True]):
-            _index = self.INDEX('path/to/index')
-            mock_walk.assert_called_once_with(
-                _index._path, extension=_index.extns, pattern=[_index._name])
-
-
-@unittest.skip('all')
-class TestIndexType__check_prefix(IndexTypeTestCase):
-
-    def test_prefix_and_name_are_None_if_dir_given(self):
-        self.setup_mock_check_extns()
-        with mock.patch('os.path.isdir', return_value=True):
-            _index = self.INDEX('foo/bar')
-        self.assertIsNone(_index._prefix)
-        self.assertIsNone(_index._name)
-
-    def test_init_sets_prefix_and_name_if_file_prefix_given(self):
-        '''Test for detection of an incomplete file name'''
-
-        self.setup_mock_check_extns()
-        with mock.patch('os.path.isdir', side_effect=[False, True]):
-            _index = self.INDEX('foo/bar')
-        self.assertEqual(_index._path, 'foo')
-        self.assertEqual(_index._prefix, 'foo/bar')
-        self.assertEqual(_index._name, 'bar')
-
-    def test_str_index_returns_given_prefix(self):
-        self.setup_mock_check_extns()
-        with mock.patch('os.path.isdir', side_effect=[False, True]):
-            _index = self.INDEX('foo/bar')
-        self.assertEqual(str(_index), 'foo/bar')
-
-#     def test_sandbox(self):
-#         _index = self.INDEX('foo/bar')
+                self.child('test/dir')
